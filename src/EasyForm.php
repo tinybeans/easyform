35ea126a49ccc740b1525b7f7909de702ce5a70d
Roy Okuwaki <info@tinybeans.dev>
2022-11-23 16:35:44 +1300

    Removed hasCpSection, hasCpSettings, and schemaVersion

--- src/EasyForm.php
+++ src/EasyForm.php
@@ -54,30 +54,6 @@ class EasyForm extends Plugin
      */
     public static $plugin;
 
-    // Public Properties
-    // =========================================================================
-
-    /**
-     * To execute your plugin’s migrations, you’ll need to increase its schema version.
-     *
-     * @var string
-     */
-    public string $schemaVersion = '2.0.0';
-
-    /**
-     * Set to `true` if the plugin should have a settings view in the control panel.
-     *
-     * @var bool
-     */
-    public bool $hasCpSettings = true;
-
-    /**
-     * Set to `true` if the plugin should have its own section (main nav item) in the control panel.
-     *
-     * @var bool
-     */
-    public bool $hasCpSection = false;
-
     // Public Methods
     // =========================================================================
 
